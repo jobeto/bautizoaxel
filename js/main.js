d82edfa35a36982cb8542b0305a7205fd33e265e
jobeto <ing.jajm@gmail.com>
2025-07-25 13:21:32 -0600

    Se agrega audio

--- js/main.js
+++ js/main.js
@@ -9,8 +9,19 @@ window.onload = function() {
 
 function showContent(){
     var divLoading = $("#loading");
-    var divContent = $("#page");
+    var divPlay = $("#divPlay");
 
     divLoading.remove();
+    divPlay.show();
+    divPlay.addClass("h-100 d-flex align-items-center justify-content-center");
+}
+
+function play(){
+    var divContent = $("#page");
+    var divPlay = $("#divPlay");
+
+    divPlay.remove();
     divContent.show();
+
+    document.getElementById("rhenne").play(); 
 }
